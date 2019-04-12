6b5d67f993384d6fa198aff0e9b73ab1deb6e002
yoopibit <yavorskyi.yurii@gmail.com>
2019-04-12 20:40:51 +0300

    init e-ink library

---
+++ src/epdif.h
@@ -0,0 +1,40 @@
+#ifndef EPDIF_H
+#define EPDIF_H
+
+#include <Arduino.h>
+#include <SPI.h>
+#include "imagedata.h"
+
+#if defined( ESP32 )
+// Pin definition
+#define RST_PIN         16
+#define DC_PIN          22
+#define CS_PIN          18
+#define BUSY_PIN        23
+
+#elif defined( AVR )
+#define RST_PIN         8
+#define DC_PIN          9
+#define CS_PIN          10
+#define BUSY_PIN        7
+
+#elif defined( ESP8266)
+#define RST_PIN         16
+#define DC_PIN          4
+#define CS_PIN          15
+#define BUSY_PIN        5
+#endif
+
+class EpdIf {
+public:
+    EpdIf(void);
+    ~EpdIf(void);
+
+    static int  IfInit(void);
+    static void DigitalWrite(int pin, int value); 
+    static int  DigitalRead(int pin);
+    static void DelayMs(unsigned int delaytime);
+    static void SpiTransfer(unsigned char data);
+};
+
+#endif
---
+++ src/imagedata.h
@@ -0,0 +1,20 @@
+#define USE_UNO
+//#define USE_ESP8266
+//#define USE_ESP32
+
+
+
+//#define  USE_154
+#define USE_213
+//#define USE_290
+extern const unsigned char IMAGE_DATA[];
+extern const unsigned char IMAGE_DATA1[];
+extern const unsigned char IMAGE_DATA2[];
+extern const unsigned char IMAGE_BLACK[];
+extern const unsigned char IMAGE_RED[];
+extern const unsigned char IMAGE[];
+extern const unsigned char IMAGE1[];
+extern const unsigned char IMAGE2[];
+/* FILE END */
+
+
